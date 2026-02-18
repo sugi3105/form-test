93a0b161e588d34fc813a19c0a5c81b95343d1bf
sugi <tales10-orix51-saiko@outlook.jp>
2026-02-18 11:57:46 +0900

    フォーム

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,6 +18,9 @@ use App\Http\Controllers\ContactController;
 //Route::get('/', function () {
     //return view('welcome');
 //});
+Route::get('/contact', [ContactController::class, 'index']);
+Route::post('/contact/confirm', [ContactController:: class, 'confirm']);
+Route::post('/contact/thanks', [ContactController::class, 'store']);
 Route::middleware('auth')->group(function () {
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 //Route::get('/', [AuthController::class, 'index']);
