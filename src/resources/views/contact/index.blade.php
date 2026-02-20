ef288c49ffc389917c22c3904c0d2f9517ce6fb4
sugi <tales10-orix51-saiko@outlook.jp>
2026-02-20 13:38:09 +0900

    レイアウト

--- src/resources/views/contact/index.blade.php
+++ src/resources/views/contact/index.blade.php
@@ -6,20 +6,30 @@
 
 @section('content')
 <div class="container">
-    <h2>お問い合わせ</h2>
+    <h2>contact</h2>
 
     <form class="form" action="contact/confirm" method="post">
     
     @csrf
  <div class="form__group">
-        <label>お名前</label>
-        <span class="form__label--required">※</span>
-        <input type="text" name="name" value="{{ old('name') }}">
-        <p class="error">{{ $errors->first('name') }}</p>
+        <label>
+         お名前
+         <span class="form__label--required">※</span>
+        </label>
+
+        <div class="form_input">
+         <input type="text" name="name" value="{{ old('name') }}">
+         <p class="error">{{ $errors->first('name') }}</p>
+        </div>    
  </div>
 
  <div class="form__group">
-        <label>性別</label>
+        <label>
+         性別
+          <span class="form__label--required">※</span>
+        </label>
+
+        <div class="form_input">
         <label>
         <input type="radio" name="gender" value="1" @checked(old('gender') == '1' )>
         男性
@@ -30,48 +40,73 @@
         </label>
         <input type="radio" name="gender" value="3" @checked(old('gender') == '3' )>
         その他
-        </label>
-        <span class="form__label--required">※</span>
+        </label> 
         <p class="error">{{ $errors->first('gender') }}</p>
  </div>
 
  <div class="form__group">
-    <label>メールアドレス</label>
-    <span class="form__label--required">※</span>
+    <label>
+      メールアドレス
+      <span class="form__label--required">※</span>
+    </label>
+
+    <div class="form_input">
         <input type="email" name="email" value="{{ old('email') }}">
         <p class="error">{{ $errors->first('email') }}</p>
+    </div>
  </div>
 
  <div class="form__group">
-    <label>電話番号</label>
+    <label>
+      電話番号
     <span class="form__label--required">※</span>
+    </lavel>
+
+    <div class="form_input">
         <input type="text" name="tel" value="{{ old('tel') }}">
         <p class="error">{{ $errors->first('tel') }}</p>
+    </div>
  </div>
 
  <div class="form__group">
-    <label>郵便番号</label>
-    <span class="form__label--required">※</span>
+    <label>
+      郵便番号
+   <span class="form__label--required">※</span>
+   </label>
+
+   <div class="form_input">
         <input type="text" name="postcode" value="{{ old('postcode') }}">
          <p class="error">{{ $errors->first('postcode') }}</p>
+   </div>
  </div>
 
  <div class="form__group">
-    <label>住所</label>
+    <label>
+      住所
     <span class="form__label--required">※</span>
+    </label>
+
+    <div class="form_input">
         <input type="text" name="address" value="{{ old('address') }}">
-        <p class="error">{{ $errors->first('address') }}</p> 
+        <p class="error">{{ $errors->first('address') }}</p>
+    </div> 
  </div>
 
  <div class="form__group">
-    <label>建物名</label>
+    <label>
+      建物名
+    </label>
+    <div class="form_input">
         <input type="text" name="building" value="{{ old('building') }}">
+    </div>  
         
  </div>
 
  <div class="form__group">
-    <label>お問い合わせの種類</label>
-    <span class="form__label--required">※</span>
+    <label>
+      お問い合わせの種類
+   <span class="form__label--required">※</span>
+    </label>
     <select name="category_id">
         <option value="">選択してください</option>
         @foreach ($categories as $category)
