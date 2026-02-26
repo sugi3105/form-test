2b71403035c6bc7c63b882abcf8a43025417596d
sugi <tales10-orix51-saiko@outlook.jp>
2026-02-26 20:31:35 +0900

    もう少し

--- src/resources/views/contact/thanks.blade.php
+++ src/resources/views/contact/thanks.blade.php
@@ -5,16 +5,9 @@
 @endsection
 
 @section('content')
-<div class="thanks__content">
-    <div class="thanks__heading">
-    <h2>お問い合わせありがとうございました</h2>
-    </div>
+<div class="thanks-wrapper">
+  <p class="thanks-message">お問い合わせありがとうございました</p>
 
-    <div class="thanks__message">
-        内容を確認のうえ、担当者よりご連絡いたします
-    </div>
-
-    <a href="{{ route('contact.index') }}"トップへ戻る</a>
-
+  <a href="/" class="thanks-button">HOME</a>
 </div>
 @endsection
